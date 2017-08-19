35f9cfd6fee49ae2ac464ae0ebba37c2cef2c024
Anton Firszov <anton.firszov@sapagroup.com>
2017-08-19 16:08:03 +0200

    Copied all the stuff from https://github.com/antonfirsov/Imagesharp.Tests.Images. Added optimize-all.cmd.

---
+++ tools/optimize-all.cmd
@@ -0,0 +1,4 @@
+optipng.exe -o 7 ../ReferenceOutput/DetectEdgesTest/*.png
+optipng.exe -o 7 ../ReferenceOutput/JpegDecoderTests/*.png
+optipng.exe -o 7 ../ReferenceOutput/ResizeTests/*.png
+optipng.exe -o 7 ../ReferenceOutput/TestImageExtensionsTests/*.png
