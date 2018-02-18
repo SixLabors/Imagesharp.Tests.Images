65db1d045e74e7702ec33b44f88f97b4bf86f1ea
Anton Firszov <antonfir@gmail.com>
2018-02-18 22:29:24 +0100

    reference images for GifDecoderTests

--- tools/optimize-all.cmd
+++ tools/optimize-all.cmd
@@ -3,3 +3,4 @@ optipng.exe -o 7 ../ReferenceOutput/JpegDecoderTests/*.png
 optipng.exe -o 7 ../ReferenceOutput/ResizeTests/*.png
 optipng.exe -o 7 ../ReferenceOutput/TestImageExtensionsTests/*.png
 optipng.exe -o 7 ../ReferenceOutput/AffineTransformTests/*.png
+optipng.exe -o 7 ../ReferenceOutput/GifDecoderTests/**/*.png
