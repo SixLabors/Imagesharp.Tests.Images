5a66c9c6da02bf27345f90adc05d415c0d0450ea
James Jackson-South <james_south@hotmail.com>
2018-03-21 11:50:45 +1100

    Add porter-duff reference files.

--- tools/optimize-all.cmd
+++ tools/optimize-all.cmd
@@ -4,3 +4,4 @@ optipng.exe -o 7 ../ReferenceOutput/ResizeTests/*.png
 optipng.exe -o 7 ../ReferenceOutput/TestImageExtensionsTests/*.png
 optipng.exe -o 7 ../ReferenceOutput/AffineTransformTests/*.png
 optipng.exe -o 7 ../ReferenceOutput/GifDecoderTests/**/*.png
+optipng.exe -o 7 ../ReferenceOutput/PorterDuffCompositorTests/**/*.png
