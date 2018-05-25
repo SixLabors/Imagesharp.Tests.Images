eb40b3c039dd8c8ca448cb8073a59ca178901e9f
Anton Firszov <antonfir@gmail.com>
2018-05-26 00:25:41 +0200

    reference images for DrawText

--- tools/optimize-all.cmd
+++ tools/optimize-all.cmd
@@ -7,3 +7,4 @@ optipng.exe -o 7 ../ReferenceOutput/GifDecoderTests/**/*.png
 optipng.exe -o 7 ../ReferenceOutput/PorterDuffCompositorTests/**/*.png
 optipng.exe -o 7 ../ReferenceOutput/ProjectiveTransformTests/**/*.png
 optipng.exe -o 7 ../ReferenceOutput/Drawing/GradientBrushes/**/*.png
+optipng.exe -o 7 ../ReferenceOutput/Drawing/Text/**/*.png
